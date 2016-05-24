a7ec482d4bb1c700111f7b5bdc32416351c7dd81
tiago <tiago.feitosa@fotosensores.com>
2016-05-24 11:48:58 -0300

    Change font-color on error and ok messages

--- service-foo.sh
+++ service-foo.sh
@@ -9,12 +9,11 @@ PATH="$PATH:/usr/bin:/bin"
 status() {
   PROCCESS=`ps aux | grep foo.jar | grep java | grep -v grep`
   if [ "$PROCCESS" == "" ]; then
-    echo "0" 
+    echo "0"
   else
     PID=`echo $PROCCESS_WD | cut -d " " -f2`
     echo "$PID"
   fi
-
 }
 
 start() {
@@ -73,45 +72,115 @@ restart() {
 
 install() {
   # Create necessary directories
+  echo "Creating directories tree ..."
   mkdir -p /opt/services/foo/log/
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Copy all files in installation package to the path of the service
+  echo "Copying service files to the service-foo directory ..."
   cp * /opt/services/foo/
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Copy the service to /usr/bin
+  echo "Copying service-foo script to /usr/bin ..."
   cp service-foo.sh /usr/bin/service-foo
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Copy the service to /etc/init.d
+  echo "Copying service-foo script to /etc/init.d ..."
   cp service-foo.sh /etc/init.d/service-foo
-
-  # Copy logrotate to /etc/logrotate.d
-  cp foo /etc/logrotate.d/
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Included on the system startup
+  echo "Registering the service-foo to start on startup system ..."
   update-rc.d service-foo defaults
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
+
+  # Copy logrotate to /etc/logrotate.d
+  echo "Copying logrotate configuration file to /etc/logrotate.d ..."
+  cp foo /etc/logrotate.d/
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 }
 
 uninstall() {
   # Stopping the service
+  echo "Stopping the service-foo ..."
   stop
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Removing the service of /usr/bin
+  echo "Removing the service-foo from /usr/bin ..."
   rm -rf /usr/bin/service-foo
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
-  # Removing the service of /etc/init.d
+  # Removing the service from /etc/init.d
+  echo "Removing the service-foo from /etc/init.d ..."
   rm -rf /etc/init.d/service-foo
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
-  # Excluding service dir
-  cd /opt/services/foo/
-  rm -rf *.sh
-  cd ..
-  rm -rf foo/
+  # Unregistering the service-foo to start on startup system
+  echo "Unregistering the service-foo to start on startup system ..."
+  update-rc.d service-foo remove
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
   # Removing foo from /etc/logrotate.d
+  echo "Removing foo from /etc/logrotate.d"
   rm -rf /etc/logrotate.d/foo
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 
-  # Remove of the system startup
-  update-rc.d service-foo remove
+  # Excluding service dir
+  echo "Excluding service-foo directory ..."
+  cd /opt/services/
+  rm -rf foo/
+  if [ $? -eq 0 ]; then
+    echo -e "\033[01;32m[  OK  ]\033[00;37m"
+  else
+    echo -e "\033[01;31m[ ERRO ]\033[00;37m"
+  fi
 }
 
 case $1 in
